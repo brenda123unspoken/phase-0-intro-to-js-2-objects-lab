f18c00782a1afe803c1bea8710cf314c700d65fd
Brenda Mukaya <brendamukaya@gmail.com>
2024-04-08 04:19:14 +0300

    modified to be git pushed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+let describeEmployees = () => {
+    // Function logic to describe employees
+};
+
+let updateEmployeeWithKeyAndValue = (employee, key, value) => ({ ...employee, [key]: value });
+var employee = { streetAddress: '10 Wall Street' };
+
+var updatedEmployee = { ...employee, streetAddress: '11 Broadway' };
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+var employee = {
+    name: 'Sam',
+    streetAddress: '10 Downing Street'
+};
+
+var updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
